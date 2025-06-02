0a02a692b0594e52dea4c17736cc4299bd3e41f9
Petrisor Craciun <petrisor.craciun34@gmail.com>
2025-06-02 12:51:43 +0300

    fix

--- config_flow.py
+++ config_flow.py
@@ -60,7 +60,7 @@ class ImmichConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class ImmichOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
+        super().__init__(config_entry)
 
     async def async_step_init(self, user_input: Optional[Dict[str, Any]] = None) -> Any:
         errors = {}
